e4d1a1ffc62729a21ed30bb8d282f92f72c85634
Anup Kumar <anup.raj1989@gmail.com>
2020-04-18 12:17:51 +0530

    refactor: remove unnecessary RunWith config of JUnitPlatform

--- acceptance-test/src/test/java/packageName/AcceptanceTest.java
+++ acceptance-test/src/test/java/packageName/AcceptanceTest.java
@@ -6,8 +6,6 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,7 +16,6 @@ import packagename.domain.port.ObtainExample;
 import packagename.domain.port.RequestExample;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 public class AcceptanceTest {
 
   @Test
