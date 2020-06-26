56c267330eb33fc0c8bf8009cc2a924bce252c37
Paul Williams <paul58914080@gmail.com>
2020-06-26 21:45:53 +0530

    fix: added CucumberContextConfiguration and updated tags
    Signed-off-by: Paul Williams <paul58914080@gmail.com>

--- acceptance-test/src/test/java/packagename/cucumber/RunCucumberExampleTest.java
+++ acceptance-test/src/test/java/packagename/cucumber/RunCucumberExampleTest.java
@@ -6,8 +6,8 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features", strict = true, plugin = {
-    "json:target/cucumber/example.json", "json:target/cucumber/example.xml"}, tags = {
-    "@Example"}, glue = "classpath:packagename.cucumber")
+    "json:target/cucumber/example.json", "json:target/cucumber/example.xml"}, tags =
+    "@Example", glue = "classpath:packagename.cucumber")
 public class RunCucumberExampleTest {
 
 }
