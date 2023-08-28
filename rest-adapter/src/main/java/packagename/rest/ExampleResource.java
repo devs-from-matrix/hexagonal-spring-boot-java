e331fd513070459f7e0042012c683669321960bf
Paul Williams <paul58914080@gmail.com>
2023-08-28 13:10:42 +0530

    refactor(rest-adapter): remove unwanted annotations

--- rest-adapter/src/main/java/packagename/rest/ExampleResource.java
+++ rest-adapter/src/main/java/packagename/rest/ExampleResource.java
@@ -1,9 +1,7 @@
 package packagename.rest;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import packagename.domain.model.Example;
 import packagename.domain.port.RequestExample;
@@ -11,7 +9,6 @@ import packagename.rest.generated.api.ExampleApi;
 import packagename.rest.generated.model.ExampleInfo;
 
 @RestController
-@RequestMapping("/api/v1/examples")
 public class ExampleResource implements ExampleApi {
 
   private final RequestExample requestExample;
@@ -20,12 +17,10 @@ public class ExampleResource implements ExampleApi {
     this.requestExample = requestExample;
   }
 
-  @GetMapping
   public ResponseEntity<ExampleInfo> getExamples() {
     return ResponseEntity.ok(ExampleInfo.builder().examples(requestExample.getExamples()).build());
   }
 
-  @GetMapping("/{code}")
   public ResponseEntity<Example> getExampleByCode(@PathVariable Long code) {
     return ResponseEntity.ok(requestExample.getExampleByCode(code));
   }
