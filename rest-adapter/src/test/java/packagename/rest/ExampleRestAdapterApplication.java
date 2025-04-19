a9198a50f428358f6d743e966c28a90ea1b41020
Paul Williams <paul58914080@gmail.com>
2025-04-20 01:02:06 +0530

    refactor(rest): replace MockBean with MockitoBean

--- rest-adapter/src/test/java/packagename/rest/ExampleRestAdapterApplication.java
+++ rest-adapter/src/test/java/packagename/rest/ExampleRestAdapterApplication.java
@@ -2,15 +2,11 @@ package packagename.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
-import packagename.domain.port.RequestExample;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "packagename")
 public class ExampleRestAdapterApplication {
-
-  @MockBean private RequestExample requestExample;
 
   public static void main(String[] args) {
     SpringApplication.run(ExampleRestAdapterApplication.class, args);
